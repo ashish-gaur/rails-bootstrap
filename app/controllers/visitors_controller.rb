24bc0e4dc984fe8b8e05812e67951a55e3ec8390
ashish-gaur <ashish.gaur@joshlabs.in>
2015-01-21 18:32:05 +0530

    Removed testing code

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,5 @@
 class VisitorsController < ApplicationController
 
   def index
-    puts '$' * 100
-    puts Rails.application.config.assets.paths.inspect
-    puts Rails.application.config.assets.precompile.inspect
   end
 end
